5e75fcdc6caf902976da971b8ae7abee9f1b79bd
labue20 <labuewilfred@gmail.com>
2021-05-05 11:16:58 -0500

    create the side bar

---
+++ discord-clone/src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className ="sidebar">
+            <h2>I'm the sidebar</h2>
+            
+            <div className="sidebar_top">
+                <h3>will programmer</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
